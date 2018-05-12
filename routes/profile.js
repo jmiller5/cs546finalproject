7c5cae9af83d8a9fb7c5f3cc12947d2d5db4b794
jmiller5 <jmiller5@stevens.edu>
2018-05-11 23:32:42 -0400

    Update profile.js
    Allows adding a custom activity

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,12 @@
+const dbConnection = require("../config/mongoConnection");
 const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const activityTypeData = data.activityTypes;
 const userActivitiesData = data.userActivities;
+const Field = require("../classes/Field");
+const uuidv1 = require('uuid/v1');
+
 
 
 router.get("/profile", async (req, res) => {
@@ -29,13 +33,25 @@ router.get("/profile/addactivity", async (req, res) => {
 router.post("/profile/addactivity", async (req, res) => {
     let test = req.body;
     test.userId = req.session.user._id;
-    try{
-        const activityList = await userActivitiesData.addUserActivity(test);
-        res.render("main/addactivity", {activities:activityList, msg: "Activity Successfully Added"});
+	if('name' in test){
+		let fields = [new Field(uuidv1(), "location", "Where did the activity take place", "String", true, true),
+        new Field(uuidv1(), "Date", "Date of the Activity", "Date", true, true),
+        new Field(uuidv1(), "Distance", "How many miles did you travel on your activity?", "Number", true, true),
+		new Field(uuidv1(), "Duration", "How many minutes did you do your activity for?", "Number", true, true)];
+    console.log(fields);
+    let activity = await activityTypeData.addActivtyType(test.name, test.description, true, fields);
+	res.redirect("/profile");
+	
+	}
+	else {
+		try{
+			const activityList = await userActivitiesData.addUserActivity(test);
+			res.redirect("/profile");
 
-    } catch (e) {
-        res.status(500).send();
-    }
+		} catch (e) {
+			res.status(500).send();
+		}
+	}
 
 
 });
